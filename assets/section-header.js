bbf05dd4c36924aeb845a365f5a576a21a069a60
Nemo Rober <nemo.rober@gmail.com>
2021-11-23 15:34:03 +0100

    Full Header 1.1

--- assets/section-header.js
+++ assets/section-header.js
@@ -1,5 +1,3 @@
-console.log("Header");
-
 const header = document.querySelector("#shopify-section-header");
 
 const headerWrapper = document.querySelector(".header-wrapper");
@@ -7,9 +5,13 @@ const headerWrapper = document.querySelector(".header-wrapper");
 window.addEventListener("scroll", (e) => {
   if (window.scrollY > 20) {
     header.style.top = "0px";
-    headerWrapper.classList.remove("transparent");
+    if (window.location.pathname === "/") {
+      headerWrapper.classList.remove("transparent");
+    }
   } else {
     header.style.top = "42px";
-    headerWrapper.classList.add("transparent");
+    if (window.location.pathname === "/") {
+      headerWrapper.classList.add("transparent");
+    }
   }
 });
