17bd7020f4c0b5a54d4d93bc79ba710c04e17d0f
Nemo Rober <nemo.rober@gmail.com>
2022-01-10 15:25:06 +0100

    about page v1

--- assets/section-header.js
+++ assets/section-header.js
@@ -5,13 +5,19 @@ const headerWrapper = document.querySelector(".header-wrapper");
 window.addEventListener("scroll", (e) => {
   if (window.scrollY > 20) {
     header.style.top = "0px";
-    if (window.location.pathname === "/") {
+    if (
+      window.location.pathname === "/" ||
+      window.location.pathname === "/pages/about-us"
+    ) {
       headerWrapper.classList.remove("transparent");
       headerWrapper.style.opacity = 1;
     }
   } else {
     header.style.top = "42px";
-    if (window.location.pathname === "/") {
+    if (
+      window.location.pathname === "/" ||
+      window.location.pathname === "/pages/about-us"
+    ) {
       headerWrapper.classList.add("transparent");
       headerWrapper.style.opacity = 1;
     }
