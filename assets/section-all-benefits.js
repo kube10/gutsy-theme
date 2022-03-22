13ba9c7e14c71ee39503c1b7297e9ccf2aad5ef6
Nemo Rober <nemo.rober@gmail.com>
2022-03-22 16:30:10 +0100

    Bowl 2

--- assets/section-all-benefits.js
+++ assets/section-all-benefits.js
@@ -2,42 +2,112 @@
   const dogBowl = document.getElementById("dog-bowl-svg");
   const korrels = dogBowl.querySelectorAll(".korrel");
 
+  const transformArray = [];
+
   korrels.forEach((korrel, i) => {
     const viewPortOffset = korrel.getBoundingClientRect();
     const left = viewPortOffset.left;
 
+    let offsetX;
+
+    let offsetY = 500 * (1 + i * 0.02);
+
     if (left > window.innerWidth / 2) {
-      const offset = left - window.innerWidth / 2;
-      korrel.style.transform = "translate(-" + offset + "px, -250px)";
+      offsetX = (left - window.innerWidth / 2) * -1;
+      const rndm = Math.random();
+      const rndm2 = Math.random();
+      if (rndm2 > 0.5) {
+        offsetX = offsetX * (1 + rndm);
+      } else {
+        offsetX = offsetX * (1 - rndm);
+      }
+      if (rndm > 0.5) {
+        offsetY = offsetY * (1 + rndm2);
+      } else {
+        offsetY = offsetY * (1 - rndm2);
+      }
+      korrel.style.transform = `translate(${offsetX}px, -${offsetY}px)`;
     } else {
-      const offset = (left - window.innerWidth / 2) * -1;
-      korrel.style.transform = "translate(" + offset + "px, -250px)";
+      offsetX = (left - window.innerWidth / 2) * -1;
+      const rndm = Math.random();
+      const rndm2 = Math.random();
+      if (rndm2 > 0.5) {
+        offsetX = offsetX * (1 + rndm);
+      } else {
+        offsetX = offsetX * (1 - rndm);
+      }
+      if (rndm > 0.5) {
+        offsetY = offsetY * (1 + rndm2);
+      } else {
+        offsetY = offsetY * (1 - rndm2);
+      }
+      korrel.style.transform = `translate(${offsetX}px, -${offsetY}px)`;
     }
+
+    transformArray.push({
+      id: i,
+      baseX: offsetX,
+      baseY: offsetY,
+    });
   });
 
-  const korrelFragments = Math.ceil(korrels.length / 40);
-  const korrelsWrap = document.getElementById("korrels");
-  const amountPerFragment = Math.ceil(korrels.length / korrelFragments);
-
   window.onscroll = () => {
-    const scrollY = window.scrollY;
-    const rate = window.scrollY / window.innerHeight;
+    const scrollY = window.scrollY * 2;
+    let rate = scrollY / window.innerHeight;
     korrels.forEach((korrel, i) => {
-      korrel.classList.add("show");
-      const baseY = -250;
-      const baseX = korrel.style.transform.substring(
-        10,
-        korrel.style.transform.indexOf("px")
-      );
+      // const transform = korrel.style.transform;
+      // const valuesString = transform.substring(9);
+      // const valuesSubstr = valuesString.substring(1, valuesString.indexOf(")"));
+      // const valuesArray = valuesSubstr.split(",");
+      // const valuesArrayInt = [];
+      // valuesArray.forEach((value, i) => {
+      //   valuesArrayInt.push(parseInt(value.substring(0, value.indexOf("p"))));
+      // });
+
+      if (rate > 1) {
+        rate = 1;
+      }
+
+      const baseX = transformArray[i].baseX;
+      const baseY = transformArray[i].baseY;
 
       const rateY = baseY - baseY * rate;
       const rateX = baseX - baseX * rate;
 
-      // korrel.style.transform = "translate(" + Math.ceil(baseY * rate) + "px)";
-      korrel.style.transform = `translate(${rateX}px, ${rateY}px)`;
+      korrel.style.transform = `translate(${rateX}px, -${rateY}px)`;
+
+      const viewPortOffsetKorrel = korrel.getBoundingClientRect();
+      const leftKorrel = viewPortOffsetKorrel.left;
+      const topKorrel = viewPortOffsetKorrel.top;
+      const rightKorrel = viewPortOffsetKorrel.right;
+      const bottomKorrel = viewPortOffsetKorrel.bottom;
+
+      const viewPortOffsetBowl = dogBowl.getBoundingClientRect();
+      const leftBowl = viewPortOffsetBowl.left;
+      const topBowl = viewPortOffsetBowl.top;
+      const rightBowl = viewPortOffsetBowl.right;
+      const bottomBowl = viewPortOffsetBowl.bottom;
+
+      if (scrollY < 100) {
+        korrel.classList.remove("show");
+      } else {
+        if (
+          leftKorrel > leftBowl &&
+          rightKorrel < rightBowl &&
+          topKorrel - 50 > topBowl &&
+          bottomKorrel < bottomBowl
+        ) {
+          korrel.classList.add("show");
+        } else {
+          korrel.classList.remove("show");
+        }
+      }
     });
   };
 
+  // const korrelFragments = Math.ceil(korrels.length / 40);
+  // const korrelsWrap = document.getElementById("korrels");
+  // const amountPerFragment = Math.ceil(korrels.length / korrelFragments);
   // for (var i = 0; i < korrelFragments; i++) {
   //   const index = i;
   //   let delay;
